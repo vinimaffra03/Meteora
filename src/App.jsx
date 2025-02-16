81505574e25bf491e986b470123bf7055ee3cd98
vinimaffra03 <viniciushmdem15@gmail.com>
2025-02-16 17:47:18 -0300

    all cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
 import react from 'react'
 import Navbar from './components/Navbar'
 import Carousel from './components/Carousel'
+import Cards from './components/Cards'
+import Products from './components/Products'
 
 function App() {
   return (
     <>
       <div>
         <Navbar label='METEORA'/>
+
         <Carousel />
+
+        <h2 class="text-center" >Busque por categoria :</h2>
+
+        <Cards />
+        <Cards />
+        <Cards />
+        <Cards />
+        <Cards />
+        <Cards />
+
+        <h2 class="text-center">Produtos que estão bombando!</h2>
+
+        <Products />
+        <Products />
+        <Products />
+        <Products />
+        <Products />
+        <Products />
       </div>
     </>
   )
